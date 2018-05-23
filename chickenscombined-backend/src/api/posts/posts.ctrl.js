32641a00a1c3306d057e825e6ca52bc2922214cd
moo25 <ansckdan@gmail.com>
2018-05-23 23:09:52 +0900

    connected to MongoDB server using mongoose

--- chickenscombined-backend/src/api/posts/posts.ctrl.js
+++ chickenscombined-backend/src/api/posts/posts.ctrl.js
@@ -3,86 +3,114 @@ let postId = 1;
 const posts = [
   {
     id: 1,
-    title: "치킨이름",
-    body: "치킨설명"
+    title: "제목",
+    body: "내용"
   }
 ];
-
-// 치킨 포스트 write POST /api/posts
+/*----------------------------------------------------------- */
+/* 1. 포스트 작성
+   POST /api/posts
+   { title, body } */
 exports.write = ctx => {
-  console.log("hihihihihihih");
   const { title, body } = ctx.request.body;
+
   postId += 1;
+
   const post = { id: postId, title, body };
-  posts.push(post); //
+  posts.push(post); // 업데이트
   ctx.body = post;
 };
 
-// 치킨 포스트 list 조회 GET /api/posts */
+/*----------------------------------------------------------- */
+/* 2. 포스트 목록 조회
+   GET /api/posts */
 exports.list = ctx => {
   ctx.body = posts;
 };
 
-// 특정 치킨 포스트 조회 GET /api/posts/:id
+/*----------------------------------------------------------- */
+/* 3. 특정 포스트 조회
+   GET /api/posts/:id */
 exports.read = ctx => {
   const { id } = ctx.params;
+
   const post = posts.find(p => p.id.toString() === id);
+
   if (!post) {
     ctx.status = 404;
     ctx.body = {
-      message: "치킨이 존재하지 않습니다"
+      message: "치킨 포스트가 존재하지 않습니다."
     };
     return;
   }
   ctx.body = post;
 };
 
+/*----------------------------------------------------------- */
+/* 4. 특정 포스트 제거
+   DELETE /api/posts/:id */
 exports.remove = ctx => {
   const { id } = ctx.params;
+
   const index = posts.findIndex(p => p.id.toString() === id);
+
   if (index === -1) {
     ctx.status = 404;
     ctx.body = {
-      message: "치킨이 존재하지 않습니다"
+      message: "치킨 포스트가 존재하지 않습니다."
     };
     return;
   }
+
   posts.splice(index, 1);
-  ctx.status = 204;
+  ctx.status = 204; // No Content
 };
 
-// 포스트 수정,교체 PUT /api/posts/:id
+/*----------------------------------------------------------- */
+/* 5.포스트 수정 (교체)
+  PUT /api/posts/:id
+  { title, body } */
 exports.replace = ctx => {
   const { id } = ctx.params;
+
   const index = posts.findIndex(p => p.id.toString() === id);
+
   if (index === -1) {
     ctx.status = 404;
     ctx.body = {
-      message: "치킨이 존재하지 않습니다"
+      message: "치킨 포스트가 존재하지 않습니다."
     };
     return;
   }
+
   posts[index] = {
     id,
-    ...ctx.requestbody
+    ...ctx.request.body
   };
   ctx.body = posts[index];
 };
 
-// 포스트 수정 PATCH /api/posts/:id
+/*----------------------------------------------------------- */
+/* 6. 포스트 수정 (특정 필드 변경)
+   PATCH /api/posts/:id
+   { title, body } */
 exports.update = ctx => {
   const { id } = ctx.params;
+
   const index = posts.findIndex(p => p.id.toString() === id);
+
   if (index === -1) {
     ctx.status = 404;
     ctx.body = {
-      message: "치킨이 존재하지 않습니다"
+      message: "치킨 포스트가 존재하지 않습니다."
     };
     return;
   }
+
   posts[index] = {
     ...posts[index],
     ...ctx.request.body
   };
   ctx.body = posts[index];
 };
+/*----------------------------------------------------------- */
--- chickenscombined-backend/src/index.js
+++ chickenscombined-backend/src/index.js
@@ -1,15 +1,30 @@
+require("dotenv").config();
 const Koa = require("koa");
 const Router = require("koa-router");
 const bodyParser = require("koa-bodyparser");
-
+const mongoose = require("mongoose");
 const api = require("./api");
+
+const { PORT: port = 4000, MONGO_URI: mongoURI } = process.env;
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(mongoURI)
+  .then(() => {
+    console.log("connected to mongoDB");
+  })
+  .catch(e => {
+    console.error(e);
+  });
 
 const app = new Koa();
 const router = new Router();
 
-router.use("/api", api.routes()); // api route
+router.use("/api", api.routes());
+
 app.use(bodyParser());
 app.use(router.routes()).use(router.allowedMethods());
-app.listen(4000, () => {
-  console.log("listenling to port 4000!");
+
+app.listen(port, () => {
+  console.log(`listenling to port ${port}`);
 });
